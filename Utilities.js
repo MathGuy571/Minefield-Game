d3daa147f8bf9d1e66d8cc66f43cceed9c453fe4
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2019-11-09 23:12:45 +0200

    updated line and circle functions

--- Utilities.js
+++ Utilities.js
@@ -6,20 +6,21 @@ const calcFPS = () => {
     fps.innerText = `fps: ${round(1000/timeDiff)}`;
 };
 
-const line = (x1, y1, x2, y2, color) => {
+const line = (x1, y1, x2, y2, color, width=1) => {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
+    ctx.lineWidth = width;
     ctx.stroke();
     ctx.closePath();
 };
 
-const circle = (x, y, r, color) => {
+const circle = (x, y, r, color, stroke = true) => {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*π);
     ctx.fillStyle = color;
-    ctx.fill();
+    stroke? ctx.stroke() : ctx.fill();
     ctx.closePath();
 };
 
