e6c0b70dcfed2951ade77818b5cb7a74e7ccd346
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2020-04-13 00:03:00 +0300

    Minor edit at draw method

--- Mine object.js
+++ Mine object.js
@@ -25,6 +25,6 @@ class Mine {
     }
     
     draw() {
-        circle(this.p.x, this.p.y, this.r, 0, 2*Math.PI, 1, false, "rgb(255, 0, 0)");
+        circle(this.p.x, this.p.y, this.r, 0, 2*Math.PI, false);
     }
 }
