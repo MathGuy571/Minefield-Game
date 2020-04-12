5ae91d8397fbfcc5be7916e3f54180d31490fe96
MathGuy571 <52160373+MathGuy571@users.noreply.github.com>
2020-04-13 00:03:49 +0300

    Minor edit at draw method

--- Goal object.js
+++ Goal object.js
@@ -52,6 +52,6 @@ class Goal {
     }
 
     draw() {
-        circle(this.p.x, this.p.y, this.r, 0, 2*Math.PI, 1, false, "rgb(0, 255, 0)");
+        circle(this.p.x, this.p.y, this.r, 0, 2*Math.PI, false);
     }
 }
